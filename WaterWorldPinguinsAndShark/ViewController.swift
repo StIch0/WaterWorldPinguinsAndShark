db820b8ca80ba0eac63220f0b5d3c97f9b4a2f6f
StIch0 <you@example.com>
2018-05-17 22:35:19 +0800

    Initial Commit

---
+++ WaterWorldPinguinsAndShark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaterWorldPinguinsAndShark
+//
+//  Created by Pavel Burdukovskii on 17/05/2018.
+//  Copyright © 2018 Pavel Burdukovskii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
